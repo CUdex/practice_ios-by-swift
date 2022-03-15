7f425e300c28f16c47dba39fac136695a6fcbe1b
System Administrator <root@lyuchan-ul-ui-MacBookPro.local>
2022-03-15 22:50:28 +0900

    start weather app

---
+++ weatherTableApp/weatherTableApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  weatherTableApp
+//
+//  Created by cudex on 2022/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
