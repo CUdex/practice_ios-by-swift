ed6d65ca714940539265b8113e16f6b61fd1fa1e
System Administrator <root@lyuchan-ul-ui-MacBookPro.local>
2022-03-20 01:01:18 +0900

    start constellation

---
+++ constellaionApp/constellaionApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  constellaionApp
+//
+//  Created by cudex on 2022/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dogButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
